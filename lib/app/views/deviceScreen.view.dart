439d6137c9d676c8a3851cb1fb5c2bcad2e6e8df
João Antonio Ferreira Neto <joao.neto@dtidigital.com.br>
2021-06-13 13:56:29 -0300

    Bluetooth agora vai pra caralho

--- lib/app/views/deviceScreen.view.dart
+++ lib/app/views/deviceScreen.view.dart
@@ -1,18 +1,15 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
-import 'package:flutter_blue/gen/flutterblue.pbjson.dart';
-import 'package:flutter_blue_example/app/widgets/characteristic_tile.widget.dart';
-import 'package:flutter_blue_example/app/widgets/descriptor_tile.widget.dart';
-import 'package:flutter_blue_example/app/widgets/service_tile.widget.dart';
 
 class DeviceScreen extends StatelessWidget {
   const DeviceScreen({Key? key, required this.device}) : super(key: key);
 
   final BluetoothDevice device;
 
-  void _buildServiceTiles(BluetoothService services) async {
-    for(var c in services.characteristics ){
+  void _buildServiceTiles(List<BluetoothService> services) async {
+    var service = services.firstWhere((x) => x.uuid == Guid('9e98d7aF-d2f9-42f5-acd2-bcb5a5cdc7df'));
+    
+    for(var c in service.characteristics ){
       await c.write([0x7f], withoutResponse: true);
     }
   }
@@ -104,7 +101,7 @@ class DeviceScreen extends StatelessWidget {
                     textStyle: const TextStyle(fontSize: 20),
                     onSurface: Colors.blue,
                   ),
-                  onPressed: (){_buildServiceTiles(snapshot.data!.firstWhere((x) => x.uuid == Guid('9e98d7aF-d2f9-42f5-acd2-bcb5a5cdc7df')));},
+                  onPressed: (){_buildServiceTiles(snapshot.data!);},
                   child: const Text("Abrir"),
                 );
               },
